b708bf89c5f3e5e37b351450969a3b028d1cdbe2
divad9 <davidbabayo94@gmail.com>
2026-01-05 22:08:57 +0100

    toh

---
+++ docs/app.py
@@ -0,0 +1,11 @@
+"""
+User Service Entry Point
+"""
+import os
+from app import create_app
+
+app = create_app()
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5001))
+    app.run(host='0.0.0.0', port=port, debug=True)
